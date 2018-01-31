85a44082f72fad552f062e43735c96a48f8a4b32
Aaron Earl <com_tech12@yahoo.com>
2018-01-31 12:22:05 -0800

    fixed up script

--- HelloWorld/HelloWorld/App_Data/UP.sql
+++ HelloWorld/HelloWorld/App_Data/UP.sql
@@ -36,7 +36,7 @@ CREATE TABLE dbo.Record
 	ID			INT NOT NULL,
 	AthleteID	INT NOT NULL,
 	WorkoutID	INT NOT NULL,
-	WorkoutTime	TIME,	--Time in hrs:mins:secs
+	WorkoutTime	NVARCHAR,	--Time in hrs:mins:secs
 	Distance	FLOAT, --Allows for fractional miles
 	Steps		INT, 
 	HeartRate	INT, --IN BPM
