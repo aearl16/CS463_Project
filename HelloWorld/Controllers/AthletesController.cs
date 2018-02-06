6300826b40d652bb5de98bf813572373a5bf3c40
Aaron Earl <com_tech12@yahoo.com>
2018-02-05 17:31:14 -0800

    New testing development branch

---
+++ HelloWorld/Controllers/AthletesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HelloWorld.Models;
+
+namespace HelloWorld.Controllers
+{
+    public class AthletesController : Controller
+    {
+        private XCStatsContext db = new XCStatsContext();
+
+        // GET: Athletes
+        public ActionResult Index()
+        {
+            var athletes = db.Athletes.Include(a => a.Coach);
+            return View(athletes.ToList());
+        }
+
+        // GET: Athletes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Athlete athlete = db.Athletes.Find(id);
+            if (athlete == null)
+            {
+                return HttpNotFound();
+            }
+            return View(athlete);
+        }
+
+        // GET: Athletes/Create
+        public ActionResult Create()
+        {
+            ViewBag.CoachID = new SelectList(db.Coaches, "ID", "FullName");
+            return View();
+        }
+
+        // POST: Athletes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,CoachID,FullName,DOB")] Athlete athlete)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Athletes.Add(athlete);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CoachID = new SelectList(db.Coaches, "ID", "FullName", athlete.CoachID);
+            return View(athlete);
+        }
+
+        // GET: Athletes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Athlete athlete = db.Athletes.Find(id);
+            if (athlete == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CoachID = new SelectList(db.Coaches, "ID", "FullName", athlete.CoachID);
+            return View(athlete);
+        }
+
+        // POST: Athletes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,CoachID,FullName,DOB")] Athlete athlete)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(athlete).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CoachID = new SelectList(db.Coaches, "ID", "FullName", athlete.CoachID);
+            return View(athlete);
+        }
+
+        // GET: Athletes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Athlete athlete = db.Athletes.Find(id);
+            if (athlete == null)
+            {
+                return HttpNotFound();
+            }
+            return View(athlete);
+        }
+
+        // POST: Athletes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Athlete athlete = db.Athletes.Find(id);
+            db.Athletes.Remove(athlete);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
