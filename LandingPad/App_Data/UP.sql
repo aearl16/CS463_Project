03c74d7f0bf9abfa5ccc6b90c60efc7e5da88613
Aaron Earl <com_tech12@yahoo.com>
2018-02-16 15:04:38 -0800

    added UP script

---
+++ LandingPad/App_Data/UP.sql
@@ -0,0 +1,60 @@
+﻿-- User Table
+CREATE TABLE dbo.LPUser
+(
+	UserID INT NOT NULL, -- TO CONNECT TO ASP.NET IDENTITY
+	Email VARCHAR(MAX) NOT NULL,
+	Birthdate DATETIME,
+	FirstName VARCHAR(MAX) NOT NULL,
+	LastName VARCHAR(MAX) NOT NULL,
+	PhoneNumber VARCHAR(MAX),
+	Username VARCHAR(MAX),
+	CONSTRAINT [PK_dbo.Users] PRIMARY KEY (UserID)
+);
+
+-- Profile Table
+CREATE TABLE dbo.LPProfile
+(
+	ProfileID INT IDENTITY(1,1) NOT NULL,
+	UserID INT NOT NULL,
+	ProfilePhoto VARBINARY(MAX),
+	DisplayRealName BIT NOT NULL DEFAULT 0, --Defualt off
+	Friends INT,
+	Followers INT,
+	Writers INT,
+	CONSTRAINT [PK_dbo.LPProfile] PRIMARY KEY (ProfileID),
+	CONSTRAINT [FK_dbo.LPUser] FOREIGN KEY (UserID)
+	REFERENCES dbo.LPUser (UserID)
+);
+
+-- Writings Table
+CREATE TABLE dbo.Writing
+(
+	WritingID INT IDENTITY(1,1) NOT NULL,
+	FolderID INT,
+	ProfileID INT NOT NULL,
+	Title VARCHAR(MAX) NOT NULL,
+	Document VARBINARY(MAX) NOT NULL,
+	AddDate DATETIME NOT NULL,
+	EditDate DATETIME NOT NULL,
+	LikesOn BIT NOT NULL, -- THERE IS NO BOOLEAN DATA TYPE
+	CommentsOn BIT NOT NULL,
+	CritiqueOn BIT NOT NULL,
+	DocType VARCHAR(MAX) NOT NULL,
+	DescriptionText VARCHAR(MAX) NOT NULL
+	CONSTRAINT [PK_dbo.Writing] PRIMARY KEY (WritingID),
+	--CONSTRAINT [FK_dbo.FolderID] FOREIGN KEY (FolderID) --key added but left out until folder table is added
+	--REFERENCES dbo.Folder (FolderID)
+);
+
+-- Pseudonym Table
+CREATE TABLE dbo.Pseudonym
+(
+	PseudonymID INT IDENTITY(1,1) NOT NULL,
+	ProfileID INT NOT NULL,
+	Pseudonym VARCHAR(MAX) NOT NULL
+	CONSTRAINT [PK_dbo.PseudonymID] PRIMARY KEY (PseudonymID),
+	CONSTRAINT [FK_dbo.LPProfile] FOREIGN KEY (ProfileID)
+	REFERENCES dbo.LPProfile (ProfileID)
+
+)
+
