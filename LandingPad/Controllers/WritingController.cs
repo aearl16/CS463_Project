85efdf5576041faa27c37f91dc2e7044050cabcd
Aaron Earl <com_tech12@yahoo.com>
2018-02-19 16:19:39 -0800

    accidental commit on upstream Dev

---
+++ LandingPad/Controllers/WritingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LandingPad.Controllers
+{
+    public class WritingController : Controller
+    {
+        // GET: Profile
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
