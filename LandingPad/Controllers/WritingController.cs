0b148f5c01398a51f8649fdec5219aaa3b2c0697
mcalawa <mccalawa@gmail.com>
2018-02-21 12:10:32 -0800

    Updating Dev with my stuff

---
+++ LandingPad/Controllers/WritingController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LandingPad.Controllers
+{
+    public class WritingController : Controller
+    {
+        // GET: Writing
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Document()
+        {
+            return View();
+        }
+    }
+}
