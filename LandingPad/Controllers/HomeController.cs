436c65370ea67ce8642b56e86306e5bdd0b42c66
Aaron Earl <com_tech12@yahoo.com>
2018-02-27 21:34:29 -0800

    minor change to fix ==> authenticate on homepage

--- LandingPad/Controllers/HomeController.cs
+++ LandingPad/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ namespace LandingPad.Controllers
 {
     public class HomeController : Controller
     {
+        [Authorize]
         public ActionResult Index()
         {
             return View();
