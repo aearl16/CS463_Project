86fa4f794df6e3662f17fe70ce4f78437973b032
Aaron Earl <com_tech12@yahoo.com>
2018-02-27 21:13:40 -0800

    updated with Rahevin's fork

--- LandingPad/Controllers/WritingController.cs
+++ LandingPad/Controllers/WritingController.cs
@@ -7,6 +7,7 @@ using System.Web.Mvc;
 
 namespace LandingPad.Controllers
 {
+    [Authorize]
     public class WritingController : Controller
     {
         LandingPadContext db = new LandingPadContext();
