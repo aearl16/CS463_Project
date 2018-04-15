170d3c7b1b9160aefb161944af45fe0de1420553
Aaron Earl <com_tech12@yahoo.com>
2018-04-15 12:03:20 -0700

    added unit tests for file extensions

---
+++ LPTesting/LPTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using LandingPad.Controllers;
+
+namespace LPTesting
+{
+    [TestFixture]
+    public class TestFileExtensions
+    {
+
+        [Test]
+        public void TestUpload_FileExtensions_DOCX_Returns_True()
+        {
+            UploadController up = new UploadController();
+            Assert.IsTrue(up.CheckExt(".DOCX"));
+        }
+
+        [Test]
+        public void TestUpload_FileExtensions_DOC_Returns_True()
+        {
+            UploadController up = new UploadController();
+            Assert.IsTrue(up.CheckExt(".DOC"));
+        }
+
+        [Test]
+        public void TestUpload_FileExtensions_null_Returns_False()
+        {
+            UploadController up = new UploadController();
+            Assert.IsFalse(up.CheckExt(null));
+        }
+    }
+}
