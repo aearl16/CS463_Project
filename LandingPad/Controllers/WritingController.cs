39d109f54bf23d9ac034a906f346bf4297fd064a
mcalawa <mccalawa@gmail.com>
2018-04-17 22:41:08 -0700

    Edit is working

--- LandingPad/Controllers/WritingController.cs
+++ LandingPad/Controllers/WritingController.cs
@@ -7,6 +7,7 @@ using System.Web.Mvc;
 using LandingPad.Models;
 using System.Text;
 using System.Collections.ObjectModel;
+using System.Data.Entity;
 
 namespace LandingPad.Controllers
 {
@@ -47,6 +48,113 @@ namespace LandingPad.Controllers
             ViewBag.ID = id;
 
             return View(db);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(string[] FormatTags, string[] Pseudonyms, FormCollection form, [Bind(Include = "ProfileID, Title, LikesOn," +
+                "CommentsOn, CritiqueOn, DescriptionText")] Writing ed)
+        {
+            if (!ModelState.IsValid)
+            {
+                int id = Int32.Parse(form["WritingID"]);
+               
+                Writing wr = db.Writings.Find(id);
+                wr.Title = form["Title"];
+                wr.EditDate = DateTime.Now;
+                wr.LikesOn = form["LikesOn"] != null ? true : false;
+                wr.CommentsOn = form["CommentsOn"] != null ? true : false;
+                wr.CritiqueOn = form["CritiqueOn"] != null ? true : false;
+                wr.DescriptionText = form["DescriptionText"];
+                wr.Document = Encoding.Unicode.GetBytes(form["EditorContent"]);
+                db.Entry(wr).State = EntityState.Modified;
+                db.SaveChanges();
+
+                ICollection<FormatTag> allFT = db.FormatTags.ToList();
+                foreach (var item in allFT)
+                {
+                    bool isSelected = false;
+                    for(int i = 0; i < FormatTags.Length; i++)
+                    {
+                        if (item.FormatID == Int32.Parse(FormatTags[i]))
+                            isSelected = true;
+                    }
+
+                    if (db.WritingFormats.Where(w => w.WritingID == id).Where(w => w.FormatID == item.FormatID).ToList().Count > 0 && isSelected == false)
+                    {
+                        WritingFormat wf = db.WritingFormats.Where(w => w.WritingID == id).Where(w => w.FormatID == item.FormatID).First();
+                        db.WritingFormats.Remove(wf);
+                        db.SaveChanges();
+                    }
+                }
+
+                //add any format tags that don't already exist
+                foreach (var selection in FormatTags)
+                {
+                    int selected = Int32.Parse(selection);
+                    if(db.WritingFormats.Where(w => w.WritingID == id).Where(w => w.FormatID == selected).ToList().Count == 0)
+                    {
+                        WritingFormat wf = new WritingFormat()
+                        {
+                            WritingID = id,
+                            FormatID = selected
+                        };
+                        db.WritingFormats.Add(wf);
+                        db.SaveChanges();
+                    }
+                }
+
+                ICollection<Pseudonym> allPseudonyms = db.Pseudonyms.ToList();
+                foreach (var item in allPseudonyms)
+                {
+                    bool isSelected = false;
+                    for (int i = 0; i < Pseudonyms.Length; i++)
+                    {
+                        if (item.PseudonymID == Int32.Parse(Pseudonyms[i]))
+                            isSelected = true;
+                    }
+
+                    if (db.WritingPseudonyms.Where(w => w.WritingID == id).Where(w => w.PseudonymID == item.PseudonymID).ToList().Count > 0 && isSelected == false)
+                    {
+                        WritingPseudonym wp = db.WritingPseudonyms.Where(w => w.WritingID == id).Where(w => w.PseudonymID == item.PseudonymID).First();
+                        db.WritingPseudonyms.Remove(wp);
+                        db.SaveChanges();
+                    }
+                }
+
+                //add any pseudonyms that don't already exist
+                foreach (var selection in Pseudonyms)
+                {
+                    int selected = Int32.Parse(selection);
+                    if (db.WritingPseudonyms.Where(w => w.WritingID == id).Where(w => w.PseudonymID == selected).ToList().Count == 0)
+                    {
+                        WritingPseudonym wp = new WritingPseudonym()
+                        {
+                            WritingID = id,
+                            PseudonymID = selected
+                        };
+                        db.WritingPseudonyms.Add(wp);
+                        db.SaveChanges();
+                    }
+                }
+
+                //previouslySelectedPseudonyms = previouslySelectedPseudonyms.Distinct().ToList();
+                //foreach(var item in previouslySelectedPseudonyms)
+                //{
+                //    if(db.WritingPseudonyms.Where(w => w.WritingID == id).Select(w => w.PseudonymID).ToList().Contains(item))
+                //    {
+                //        WritingPseudonym wp = db.WritingPseudonyms.Where(w => w.WritingID == id).Where(w => w.PseudonymID == item).First();
+                //        db.WritingPseudonyms.Remove(wp);
+                //        db.SaveChanges();
+                //    }
+                //}
+
+                return RedirectToAction("ViewWriting", "Writing", new { @id = id });
+            }
+            else
+            {
+                ViewBag.FileStatus = "Model Invalid";
+                return View(db);
+            }
         }
 
         [HttpGet]
