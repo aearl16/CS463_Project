11cfcb0e0378ecf831b80bbb1f782bc9a8c9da94
Aaron Earl <com_tech12@yahoo.com>
2018-05-07 17:26:51 -0700

    added comments to Upload Controller

---
+++ LandingPad/Repositories/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LandingPad.Repositorys
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        ITwitterRepository Twitter { get; }
+        //add your IRepos here
+
+        int Complete();
+    }
+}
