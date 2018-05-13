3ca264daefe526c11a7b06f83d77ef1b6b08d2c8
Aaron Earl <com_tech12@yahoo.com>
2018-05-13 15:20:42 -0700

    captcha added and workin, does not work locally due to Google restrictions

--- LandingPad/Models/Writing.cs
+++ LandingPad/Models/Writing.cs
@@ -3,10 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Data.Entity.Spatial;
-using System.Globalization;
 using System.Web;
-using System.Web.Mvc;
 
 namespace LandingPad.Models
 {
